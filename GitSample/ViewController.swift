ad161b3d84fd98c296d5f0acc0e3569270049f34
k-sathireddy <k.sathireddy314@gmail.com>
2017-05-22 14:39:59 +0530

    Initial Commit

---
+++ GitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitSample
+//
+//  Created by Enterpi mini mac on 5/22/17.
+//  Copyright © 2017 Enterpi mini mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
